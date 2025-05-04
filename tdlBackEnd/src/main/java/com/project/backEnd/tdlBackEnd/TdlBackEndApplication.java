5888a977a3ae275dc221c4442ec5db82c1e0feb3
heric <herickdan@hotmail.com>
2025-05-04 20:18:22 -0300

    create database

---
+++ tdlBackEnd/src/main/java/com/project/backEnd/tdlBackEnd/TdlBackEndApplication.java
@@ -0,0 +1,11 @@
+package com.project.backEnd.tdlBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlBackEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TdlBackEndApplication.class, args);
+	}
+}
---
+++ tdlBackEnd/src/test/java/com/project/backEnd/tdlBackEnd/TdlBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.backEnd.tdlBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
